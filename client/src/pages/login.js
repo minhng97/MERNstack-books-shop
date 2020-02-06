893fd68f04c45728ea5a102d63d8c48d237d45b3
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-06 23:40:12 +0700

    Add Login function

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,45 +1,67 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios'
-
+import md5 from 'md5'
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            info: ""
         }
         this.handleChange = this.handleChange.bind(this)
         this.submit = this.submit.bind(this)
     }
 
-
-
+ 
     handleChange = ({ target }) => {
         const { name, value } = target
 
         this.setState({
             [name]: value
-    })
+        })
         console.log(this.state)
     }
+
     submit = (event) => {
         event.preventDefault()
 
         const payload = {
             username: this.state.username,
-            password: this.state.password
+            password: md5(this.state.password)
         }
-        // Save item to database
+
+console.log(md5(payload.password))
+        // Get users from database
         axios({
-            url: '/api/login',
+            url: '/api/log',
             method: 'POST',
-            data: payload
+            data: payload,
+            timeout: 70000
         })
-            .then(() => {
-                console.log('data has been sent to server');
+            .then((response) => {
+    
+                // Check if the username is already existed
+                const isValid = response.data.msg
+                console.log(isValid)
+                if (isValid === false) {
+                    this.setState({ info: "Wrong username or password" })
+                } else {
+                    this.setState({ info: "You have succesfully login!" })
+
+                    setInterval(() => {
+                        window.location.href = '/';
+                    }, 1000);
+                    
+                }
+
+                console.log(this.state)
             })
-            .catch(() => console.log('500 internal error'))
+            .catch((error) => {
+                alert('Error getting users: ', error)
+            })
+
     }
 
 
@@ -48,28 +70,32 @@ class Login extends Component {
         return (
             <div className="app">
 
-                <form onSubmit={() => this.submit()}>
-                    <div className="form-input">
-                        <input
-                            type="text"
-                            name="username"
-                            placeholder="username"
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="form-input">
-                        <input
-                            type="text"
-                            placeholder="password"
-                            name="password"
-                            onChange={this.handleChange}>
-                        </input>
-                    </div>
+                <div className="alert alert-info">
+                    {this.state.info}
+                    <form onSubmit={this.submit}>
+                        <div className="form-input">
+                            <input
+                                required
+                                type="text"
+                                name="username"
+                                placeholder="username"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-input">
+                            <input
+                                required
+                                type="text"
+                                placeholder="password"
+                                name="password"
+                                onChange={this.handleChange}>
+                            </input>
+                        </div>
 
-                    <button>Submit</button>
-                </form>
+                        <button>Login</button>
+                    </form>
 
-
+                </div>
 
             </div>
         )
