3ffdca93814c3b575b93bf76445ab093e677ca20
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-08 13:49:23 +0700

    Add logout btn and userState

--- client/src/pages/topMenu.js
+++ client/src/pages/topMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   Collapse,
   Navbar,
@@ -6,43 +6,54 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  Button
 } from "reactstrap";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { UserContext } from '../contexts/user.context'
+
 const TopMenu = props => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const context = useContext(UserContext)
+console.log(context)
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand>
-            <Link to="/">React App</Link>
-          </NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink>
-                  <Link to="/">Home</Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>
-                  <Link to="/login">login</Link>
-                </NavLink>
-              </NavItem>
-
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>
+          <Link to="/">React App</Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to="/">Home</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="/reg">Register</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="/log">Login</Link>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <p>{context.login}</p>
+      <Button type="button" color="warning" onClick={context.logout}>Logout</Button>
+    </div>
 
   );
 };
-
+console.log(typeof TopMenu)
 export default TopMenu;
