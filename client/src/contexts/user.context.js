e0f7eb1018aef56def08ffb320ce82153892b787
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-08 13:49:03 +0700

    Add user context

---
+++ client/src/contexts/user.context.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export const UserContext = React.createContext()
+
+export class UserState extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            login: null,
+            cartItems: []
+        }
+        this.addToCart = this.addToCart.bind(this)
+        this.logout = this.logout.bind(this)
+    }
+
+    addToCart(product) {
+        console.log(" Added ", product)
+        this.setState({
+            cartItems: this.state.cartItems.concat(this.product)
+        })
+    }
+
+    logout() {
+        this.setState({ login: false })
+    }
+
+    render() {
+        return <UserContext.Provider value={{
+            login: this.state.login,
+            cartItems: this.state.cartItems,
+            addToCart: this.addToCart,
+            logout: this.logout
+        }}>
+            {this.props.children}
+        </UserContext.Provider>
+    }
+}
