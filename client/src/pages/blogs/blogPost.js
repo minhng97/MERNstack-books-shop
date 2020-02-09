d1ab3f777b801b854eba9f2a7423130c3b7c2ab2
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-09 17:33:18 +0700

    Shorter code with async

---
+++ client/src/pages/blogs/blogPost.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import {
+    ListGroup, ListGroupItem
+    , Form, Input, FormGroup, Label
+} from 'reactstrap';
+import Button from 'antd/es/button';
+import 'antd/dist/antd.css';
+
+import displayBlogPost from './display.blog'
+
+class BlogPost extends Component {
+    state = {
+        title: '',
+        body: '',
+        posts: [],
+        info: null,
+        loading: false,
+        iconLoading: false,
+    }
+
+    async componentDidMount() {
+        try {
+            let { data } = await this.getBlogPost()
+            
+            this.setState({ posts: data.reverse() })
+        }
+        catch (error) {
+            alert("Error ", error)
+        }
+    }
+
+    getBlogPost = () => {
+        // Get posts from database
+        return axios.get('/api')
+    }
+
+    handleChange = ({ target }) => {
+        const { name, value } = target;
+
+        this.setState({
+            [name]: value
+        })
+    }
+    enterLoading = () => {
+        this.setState({ loading: !this.state.loading });
+    };
+
+    enterIconLoading = () => {
+        this.setState({ iconLoading: !this.state.iconLoading });
+    };
+    submit = (event) => {
+        event.preventDefault()
+        this.enterIconLoading()
+        this.enterLoading()
+        const payload = {
+            title: this.state.title,
+            body: this.state.body,
+            token: localStorage.getItem("token")
+        }
+        // Save item to database
+        axios({
+            url: '/api/save',
+            method: 'POST',
+            data: payload,
+            headers: { "authorization": `Bearer ${localStorage.getItem("token")}` }
+        })
+            .then(async () => {
+
+                setInterval(() => {
+                    this.setState(() => ({ info: null }))
+                }, 1200);
+
+                this.resetUserInput();
+                
+                let { data } = await this.getBlogPost();
+                this.setState(state => ({posts: data.reverse()}))
+
+                this.enterIconLoading()
+                this.enterLoading()
+            })
+            .catch(() => {
+                this.setState(state => ({ info: "500 internal error" }))
+
+                this.enterIconLoading()
+                this.enterLoading()
+            })
+    }
+
+    resetUserInput = () => {
+        this.setState({
+            title: "",
+            body: ""
+        });
+    }
+
+
+
+    render() {
+        //JSX
+        return (
+            <div className="app">
+                {/* // Warning */}
+                {this.state.info}
+
+                <Form onSubmit={this.submit} style={{
+                    border: "1px solid lightgray",
+                    width: "auto",
+                    padding: "10px",
+                    borderRadius: "4px",
+                    backgroundColor: "#d3d3d32e"
+                }}>
+                    <div style={{ padding: "10px", width: "50%" }}>
+                        <div className="form-input">
+                            <Input
+                                type="text"
+                                name="title"
+                                placeholder="Title"
+                                value={this.state.title}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-input">
+                            <textarea
+                                placeholder="body"
+                                name="body"
+                                rows="3"
+                                value={this.state.body}
+                                onChange={this.handleChange}>
+                            </textarea>
+                        </div>
+                        <Button type="primary" loading={this.state.loading} onClick={this.submit}>
+                            Submit
+                        </Button>
+                    </div>
+
+                </Form>
+
+                <div className="posts">
+                    <ListGroup>
+                        {displayBlogPost(this.state.posts)}
+                    </ListGroup>
+                </div>
+
+
+            </div >
+        )
+    }
+
+}
+
+export default BlogPost;
