f3482f0a53b367fc05571487a43715ddbabd3cd5
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-10 21:23:41 +0700

    Add Add to cart function

--- client/src/pages/books/display.book.js
+++ client/src/pages/books/display.book.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Col } from 'reactstrap';
+import { Button, Icon } from 'antd';
+import { CartContext } from '../../contexts/Cart.context'
 
-
-const displayBooks = bookData => {
-
+export default function (bookData) {
+    const context = useContext(CartContext)
+    console.log("cartcontext:", context)
     if (!bookData) { return null }
-    return bookData.map(book => <Col
+    return (bookData.map(book => <Col
         xs="12"
         sm="6"
         lg="3"
         offset={6}
         key={book._id}
         className="books__display">
-        <h6>{book.title}</h6>
-        <img src={book.thumbnailUrl} alt="Book descr" />
+        <img src={book.thumbnailUrl} alt="Book descr" height="140px" />
+        <h6 className="books__display--title">{book.title}</h6>
+        <p className="books__display--authors"><span>{book.authors[0]}</span></p>
         <p className="books__display--description">{book.shortDescription}</p>
-        </Col>
-    )
+
+        <Button onClick={() => context.addToCart(book._id)}
+            type="primary"
+            width="">
+            <Icon type="shopping-cart" />
+
+            Add to cart
+        </Button>
+
+
+
+
+        <br />
+    </Col>
+    ))
 }
-export default displayBooks
+// export default DisplayBooks
