ff8625f149903b41c70653c7d3d94bd1b683ab1a
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-10 21:24:22 +0700

    Add cart button

--- client/src/pages/topMenu.js
+++ client/src/pages/topMenu.js
@@ -6,20 +6,27 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  Button
+  // NavLink,
+  // Button
 } from "reactstrap";
+import { Button, Icon } from 'antd';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { CartContext } from '../contexts/Cart.context'
+
+
+
+import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { UserContext } from '../contexts/user.context'
+// import { UserContext } from '../contexts/user.context'
+
 
 const TopMenu = props => {
   const [isOpen, setIsOpen] = useState(false);
+  const context = useContext(CartContext)
 
-  const context = useContext(UserContext)
-  console.log("context from user.context: ", context)
+  // const context = useContext(UserContext)
+  // console.log("context from user.context: ", context)
 
   const toggle = () => setIsOpen(!isOpen);
   const logout = () => localStorage.removeItem("token")
@@ -28,39 +35,47 @@ const TopMenu = props => {
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand>
-          <Link to="/">React App</Link>
+          <NavLink to="/">React App</NavLink>
         </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink>
-                <Link to="/">Home</Link>
-              </NavLink>
+
+              <NavLink to="/">Home</NavLink>
+
             </NavItem>
 
             <NavItem>
-              <NavLink>
-                <Link to="/books">Books</Link>
-              </NavLink>
+
+              <NavLink to="/books"> Books</NavLink>
+
             </NavItem>
           </Nav>
           <Nav navbar>
+
+
+
             <NavItem>
-              <NavLink>
-                <Link to="/reg">Register</Link>
-              </NavLink>
+
+              <NavLink to="/reg">Register</NavLink>
+
             </NavItem>
             <NavItem>
-              <NavLink>
-                <Link to="/log">Login</Link>
-              </NavLink>
+
+              <NavLink to="/log">Login </NavLink>
+
             </NavItem>
+
+            <NavItem>
+              <Button type="primary"><Icon type="shopping-cart" />{context.cartItems.length}</Button>
+            </NavItem>
+
           </Nav>
         </Collapse>
       </Navbar>
 
-      <Button type="button" color="warning" onClick={logout}>Logout</Button>
+      <Button type="danger" color="warning" onClick={logout}>Logout</Button>
     </div>
 
   );
