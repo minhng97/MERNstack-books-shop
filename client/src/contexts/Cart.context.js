97c930178857dcace1a33c385cdb3c9c8a690d4d
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-13 23:15:03 +0700

    Add cart

--- client/src/contexts/Cart.context.js
+++ client/src/contexts/Cart.context.js
@@ -1,23 +1,44 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export const CartContext = React.createContext()
 
-export class CartProvider extends Component{
-  constructor(props){
+export class CartProvider extends Component {
+  constructor(props) {
+    console.log("Cart provider constructor")
     super(props)
-    this.state={
-      cartItems: []
+    
+    let localCart = localStorage.getItem("cart")
+    if (localCart !== 'null' && localCart !== null) {
+      localCart = localCart.split(",") // String to array
+    this.setState({ cartItems: localCart })
+    } else {
+      localCart = []
     }
+
+    this.state = {
+      cartItems: localCart
+    }
+
     this.addToCart = this.addToCart.bind(this)
   }
 
-  addToCart(product){
-    console.log( " Added ", product)
+  
+  addToCart(product) {
+    let stateItems = this.state.cartItems
+    let itemToAdd = stateItems.concat(product)
+    if(stateItems === []) { itemToAdd = product}
+
     this.setState({
-      cartItems: this.state.cartItems.concat(this.product)
+      cartItems: itemToAdd
     })
+    localStorage.setItem("cart", itemToAdd)
+
   }
-  render(){
+
+
+  render() {
+
+
     return <CartContext.Provider value={{
       cartItems: this.state.cartItems,
       addToCart: this.addToCart
