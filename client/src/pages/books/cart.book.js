e76dd54460c720d3476725d8a0732c8b005c3306
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-18 23:42:07 +0700

    Fix a href warn and shorter code

--- client/src/pages/books/cart.book.js
+++ client/src/pages/books/cart.book.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react'
-import axios from 'axios'
 import { CartContext } from '../../contexts/Cart.context'
 
 import { List, Avatar, Alert, Card } from 'antd';
 import { Container, Row, Col, Badge } from 'reactstrap';
+import { Link } from "react-router-dom";
+
+import axios from 'axios'
+
 export default (props) => {
     const [books, setBooks] = useState([])
     const cart = useContext(CartContext)
-    let counting = cart.cartItems.reduce((acc, curr, ind) => {
-        return acc + curr.quantity
-    }, 0)
-    console.log("cart: ", cart)
-    useEffect(() => {
-        getAndSetBook()
-        async function getAndSetBook() {
-            let { data } = await axios.get('/api/allbooks')
-            // copy
-            const booksFetched = [...data];
-            setBooks(booksFetched)
+    let counting = cart.cartItems.reduce((acc, curr) => acc + curr.quantity, 0)
 
-        }
+    useEffect(() => {
+
+        (async function getAndSetBook() {
+            let { data } = await axios.get('/api/allbooks')
+            setBooks(data)
+        })()
     }, [])
 
 
@@ -34,16 +32,15 @@ export default (props) => {
                             <List.Item>
                                 <List.Item.Meta
                                     avatar={<Avatar src={item.product.thumbnail} style={{ width: "90px", height: "100px", borderRadius: 0 }} />}
-                                    title={<a href="#">{item.product.title}</a>}
+                                    title={<Link to="/books">{item.product.title}</Link>}
                                     description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-
+                                    
                                 />
                                 <div><h6> <Badge color="primary">{item.quantity}</Badge></h6></div>
                             </List.Item>
                         )}
                     /></Card></Col>
                 <Col lg="4">
-                    {/* <Alert message="Continue to checkout" type="info" /> */}
                     <Card title={`Total: ${counting} items`} style={{}}>
                         <p>Price: 100.400 VND</p>
                         <p>VAT: 5% </p>
