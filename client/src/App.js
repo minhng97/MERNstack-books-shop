56ef0d1fa1f67e5483987acf4f737d4757ff3f36
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-18 23:49:05 +0700

    Add path: login, books, cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import axios from 'axios'
-import BlogPost from './pages/blogPost'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+
+import BlogPost from './pages/blogs/blogPost'
 import TopMenu from './pages/topMenu'
 import Register from './pages/register'
+import Login from './pages/login'
+import AllBooks from './pages/books/books.all'
+import BookCart from './pages/books/cart.book'
 import './App.css'
-// import { TabPane } from 'reactstrap';
+
+import { CartProvider } from "./contexts/Cart.context";
+
 
 class App extends Component {
 
 
   render() {
     //JSX
-    return (
+        return (
+
       <Router>
       <div className="app">
+      <CartProvider>
+ 
         <TopMenu />
       <Switch>
         <Route path="/" exact component={BlogPost} />
         <Route path="/reg" exact component={Register} />
+        <Route path="/log" exact component={Login} />
+        <Route path="/books" exact component={AllBooks} />
+        <Route path="/bookCart" exact component={BookCart} />
         <Route component={() => <h2 className="mt-3 text-center">404 Not Found</h2>}/>
       </Switch>
+      </CartProvider>
+     
       </div>
       </Router>
     )
