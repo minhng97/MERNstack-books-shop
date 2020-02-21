dc22a9a4a91cc574b0939c0fa9868e1bbc1cc3fa
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-21 16:02:10 +0700

    Fix some style problems

--- routes/api.js
+++ routes/api.js
@@ -1,211 +1,202 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
-const md5 = require("md5")
+const express = require('express');
 
-const BlogPost = require('../models/BlogPost')
-const User = require('../models/user')
-const Books = require('../models/books')
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const md5 = require('md5');
+
+const BlogPost = require('../models/BlogPost');
+const User = require('../models/user');
+const Books = require('../models/books');
 
 router.get('/', (req, res) => {
-
-    fetchBlogPosts()
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((error) => {
-            console.log('server error: ', error)
-        })
+  fetchBlogPosts()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log('server error: ', error);
+    });
 });
 
 router.post('/save', verify, (req, res) => {
-    console.log("Body: ", req.body)
+  console.log('Body: ', req.body);
 
 
-    const data = req.body
+  const data = req.body;
 
-    const newBlogPost = new BlogPost(data)
+  const newBlogPost = new BlogPost(data);
 
-    newBlogPost.save((error) => {
-        if (error) {
-            res.status(500).json({ msg: "Internal server error" })
-            return;
-        }
-        // BlogPost
-        return res.json({
-            msg: "We received the post"
-        });
+  newBlogPost.save((error) => {
+    if (error) {
+      res.status(500).json({ msg: 'Internal server error' });
+      return;
+    }
+    // BlogPost
+    return res.json({
+      msg: 'We received the post',
     });
-
-})
+  });
+});
 
 // USER PAGEs
 
 // Go to login page
 router.post('/log', async (req, res) => {
-    let data;
-    let arrUser;
+  let data;
+  let arrUser;
 
-    data = req.body
-    arrUser = await fetchUsersData()
+  data = req.body;
+  arrUser = await fetchUsersData();
 
-    validate()
+  validate();
 
-    function validate() {
-        arrUser = arrUser.filter(user => {
-            return user.username === data.username && user.password === data.password
-        })
+  function validate() {
+    arrUser = arrUser.filter((user) => user.username === data.username && user.password === data.password);
 
-        if (arrUser.length > 0) {
-            // Verify the data
-            jwt.sign(data.username, 'secretkey', (err, token) => {
-                if (err) {
-                    res.sendStatus(403)
-                } else {
-                    res.send({
-                        msg: true,
-                        token
-                    })
-                }
-            })
-
-
+    if (arrUser.length > 0) {
+      // Verify the data
+      jwt.sign(data.username, 'secretkey', (err, token) => {
+        if (err) {
+          res.sendStatus(403);
         } else {
-            res.send({ msg: false })
+          res.send({
+            msg: true,
+            token,
+          });
         }
+      });
+    } else {
+      res.send({ msg: false });
     }
-})
+  }
+});
 
 // Go to register page
 router.get('/reg', (req, res) => {
-    fetchUsersData()
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((error) => {
-            console.log('server error: ', error)
-            res.json({ msg: "server error" })
-        })
-})
+  fetchUsersData()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log('server error: ', error);
+      res.json({ msg: 'server error' });
+    });
+});
 
 
 router.post('/reg', async (req, res) => {
-    // Initial
-    const data = req.body
-    console.log("user from client: ", data)
-    let allUsers = []
-    let isUserInDatabase;
+  // Initial
+  const data = req.body;
 
-    // Functions start here
+  let allUsers = [];
+  let isUserInDatabase;
 
-    let userIsExist = await checkUserData()
+  // Functions start here
 
-    if (userIsExist) {
-        return res.send({ isUserExist: true })
-    } else {
-        saveUser()
-    }
+  const userIsExist = await checkUserData();
 
-    async function checkUserData() {
-        allUsers = await fetchUsersData()
-
-        isUserInDatabase = allUsers.filter(checkUser)
-
-        if (isUserInDatabase.length > 0) { return true }
-        else {
-            return false
-        }
-    }
+  if (userIsExist) {
+    return res.send({ isUserExist: true });
+  }
+  saveUser();
 
 
-    function checkUser(element) {
-        return (element.username === data.username)
-    }
+  async function checkUserData() {
+    allUsers = await fetchUsersData();
 
-    function saveUser() {
+    isUserInDatabase = allUsers.filter(checkUser);
 
-            // Save item to database
-            const { username, password } = data
+    if (isUserInDatabase.length > 0) { return true; }
 
-            const newUser = new User({
-                username,
-                password: md5(password)
-            })
+    return false;
+  }
 
-            newUser.save((error) => {
-                if (error) {
-                    res.status(500).send({ msg: "500 Internal server error" })
-                    return;
-                }
-                // User
-                return res.send({
-                    isUserExist: false
-                });
-            });
 
-        
-    }
+  function checkUser(element) {
+    return (element.username === data.username);
+  }
 
-})
+  function saveUser() {
+    // Save item to database
+    const { username, password } = data;
 
+    const newUser = new User({
+      username,
+      password: md5(password),
+    });
+
+    newUser.save((error) => {
+      if (error) {
+        res.status(500).send({ msg: '500 Internal server error' });
+        return;
+      }
+      // User
+      return res.send({
+        isUserExist: false,
+      });
+    });
+  }
+});
+
+router.post('/checkauth', verify, (req, res) => {
+  res.send();
+});
 // Books page
 
 router.get('/allbooks', (req, res) => {
-    fetchBooks()
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((error) => {
-            console.log('server error: ', error)
-        })
-})
+  fetchBooks()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      res.send({ msg: 'Error getting books' });
+      console.log('server error: ', error);
+    });
+});
 
 
 function fetchBlogPosts() {
-    return Promise
-        .resolve(BlogPost.find({}))
+  return Promise
+    .resolve(BlogPost.find({}));
 }
 
 function fetchUsersData() {
-    return Promise
-        .resolve(User.find({}))
+  return Promise
+    .resolve(User.find({}));
 }
 
 function fetchBooks() {
-    return Promise
-        .resolve(Books.find({}))
+  return Promise
+    .resolve(Books.find({}));
 }
 
 function verify(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-    console.log("req: ", req.headers)
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
+  const bearerHeader = req.headers.authorization;
 
-        // Verify the post
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
 
-
-        jwt.verify(bearerToken, 'secretkey', (err, authData) => {
-            if (err) {
-                res.sendStatus(403)
-            } else {
-
-                req.token = bearerToken;
-
-                next()
-            }
-        })
+    // Verify the post
 
 
-    } else {
-        res.json({
-            msg: 'You must login first'
-        })
-    }
+    jwt.verify(bearerToken, 'secretkey', (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        req.token = bearerToken;
+
+        next();
+      }
+    });
+  } else {
+    res.json({
+      msg: 'You must login first',
+    });
+  }
 }
 
-module.exports = router
+module.exports = router;
 
 
 // Verify the post
