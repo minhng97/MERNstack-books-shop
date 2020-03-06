d3ace48ab543b9f80da2aea5bad529b6cd59c26c
minhng97 <minh.nguyentrong97@gmail.com>
2020-03-06 22:43:41 +0700

    Change topMenu useContext to reducer redux

--- client/src/pages/topMenu.js
+++ client/src/pages/topMenu.js
@@ -1,75 +1,69 @@
-import React, { useState, useContext } from "react";
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState } from 'react';
 import {
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
-  // NavLink,
-  // Button
-} from "reactstrap";
+  NavItem
+} from 'reactstrap';
 import { Button, Icon } from 'antd';
 
-import { CartContext } from '../contexts/Cart.context'
+import { NavLink } from 'react-router-dom';
+
+import { connect } from 'react-redux'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-import { NavLink } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// import { UserContext } from '../contexts/user.context'
-
-
-const TopMenu = props => {
+const TopMenu = ({ cartItems }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const { cartItems } = useContext(CartContext)
 
-  let counting = cartItems.reduce((acc, curr, ind) => {
-    return acc + curr.quantity
-  }, 0)
+  const counting = cartItems.reduce((acc, curr) => acc + curr.quantity, 0);
   const toggle = () => setIsOpen(!isOpen);
-  const logout = () => localStorage.removeItem("token")
+  const logout = () => localStorage.removeItem('token');
 
   return (
     <>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/" style={{ color: "#007bff" }}>
+
+        <NavbarBrand href="/" style={{ color: '#007bff' }}>
           <NavItem>React App</NavItem>
         </NavbarBrand>
+
         <NavbarToggler onClick={toggle} />
+
         <Collapse isOpen={isOpen} navbar>
+
           <Nav className="mr-auto" navbar>
+
             <NavItem>
-
               <NavLink to="/">Home</NavLink>
-
             </NavItem>
 
             <NavItem>
-
               <NavLink to="/books"> Books</NavLink>
-
             </NavItem>
           </Nav>
+
           <Nav navbar>
 
-
-
             <NavItem>
-
               <NavLink to="/reg">Register</NavLink>
-
             </NavItem>
+
             <NavItem>
-
               <NavLink to="/log">Login </NavLink>
-
             </NavItem>
 
             <NavItem>
               <NavLink to="/bookcart">
-                <Button type="primary"><Icon type="shopping-cart" />{counting}</Button>
+                <Button type="primary">
+                  <Icon type="shopping-cart" />
+                  {counting}
+                </Button>
               </NavLink>
             </NavItem>
 
@@ -83,4 +77,10 @@ const TopMenu = props => {
   );
 };
 
-export default TopMenu;
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state
+  }
+}
+
+export default connect(mapStateToProps)(TopMenu)
