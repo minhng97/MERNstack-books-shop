0a956f3cc1969ae3b212e31ff6fa1f74ed0042ff
minhng97 <minh.nguyentrong97@gmail.com>
2020-03-06 22:38:42 +0700

    Change books.all useContext to reducer

--- client/src/pages/books/books.all.js
+++ client/src/pages/books/books.all.js
@@ -1,46 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import { Container, Row } from 'reactstrap';
 
 import 'antd/dist/antd.css';
 
-import DisplayBooks from './display.book'
+import DisplayBooks from './display.book';
 
+import { connect } from 'react-redux'
+import { addToCart } from '../../actions/cart.action'
 
-const AllBooks = props => {
-    const [books, setBooks] = useState([])
+const AllBooks = ({ location, addProductToCart }) => {
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-        getBooks()
-    }, []
-    )
+  const getBooks = () => {
 
-    const getBooks = () => {
+    axios.get('/api/allbooks')
+      .then(({ data }) => {
+        setBooks(data);
+      })
+      .catch((error) => {
+        console.error('Error getting posts: ', error);
+      });
+  };
+  useEffect(() => {
+    getBooks();
+  }, []);
 
-        // Get posts from database
-        axios.get('/api/allbooks')
-            .then(({ data }) => {
-                setBooks(data)
-            })
-            .catch((error) => {
-                alert('Error getting posts: ', error)
-            })
+  return (
 
-    }
+    <div className="posts">
+      {JSON.stringify(location.pathname)}
+      <Container>
+        <Row>
+          <DisplayBooks bookData={books} addToCart={addProductToCart} />
+        </Row>
+      </Container>
+    </div>
 
+  );
+};
 
-    return (
-
-        <div className="posts">
-            <Container>
-                <Row>
-                    {DisplayBooks(books)}
-                </Row>
-            </Container>
-        </div>
-
-    )
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state
+  }
 }
 
-export default AllBooks
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addProductToCart: (product) => { dispatch(addToCart(product)) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllBooks)
