40a29c84a3d0dfc8c17e7b0182baac06698fb5dc
minhng97 <minh.nguyentrong97@gmail.com>
2020-03-06 22:40:22 +0700

    Change useContext to redux reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
-
 import BlogPost from './pages/blogs/blogPost'
 import TopMenu from './pages/topMenu'
 import Register from './pages/register'
@@ -11,36 +10,32 @@ import AllBooks from './pages/books/books.all'
 import BookCart from './pages/books/cart.book'
 import './App.css'
 
-import { CartProvider } from "./contexts/Cart.context";
-
 
 class App extends Component {
 
-
   render() {
-    //JSX
-        return (
+    return (
 
       <Router>
-      <div className="app">
-      <CartProvider>
- 
-        <TopMenu />
-      <Switch>
-        <Route path="/" exact component={BlogPost} />
-        <Route path="/reg" exact component={Register} />
-        <Route path="/log" exact component={Login} />
-        <Route path="/books" exact component={AllBooks} />
-        <Route path="/bookCart" exact component={BookCart} />
-        <Route component={() => <h2 className="mt-3 text-center">404 Not Found</h2>}/>
-      </Switch>
-      </CartProvider>
-     
-      </div>
+        <div className="app">
+          <TopMenu />
+          <Switch>
+            <Route path="/" exact component={BlogPost} />
+            <Route path="/reg" exact component={Register} />
+            <Route path="/log" exact component={Login} />
+            <Route path="/books" exact component={AllBooks} />
+            <Route path="/bookCart" exact component={BookCart} />
+            <Route component={() => <h2 className="mt-3 text-center">404 Not Found</h2>} />
+          </Switch>
+
+        </div>
       </Router>
     )
   }
 
 }
 
-export default App;
+
+
+export default App
+
