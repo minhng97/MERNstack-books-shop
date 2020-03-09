d4a2a8730d38b2e5bcfc927033a33712576dc0cf
minhng97 <minh.nguyentrong97@gmail.com>
2020-03-09 10:48:21 +0700

    Cart reducer

--- client/src/reducers/Cart.reducer.js
+++ client/src/reducers/Cart.reducer.js
@@ -4,22 +4,24 @@ let localStorageCart = JSON.parse(localStorage.getItem("cart")) || []
 const initialState = localStorageCart
 
 
-const reducer = (state = initialState, action) => {
+const cartReducer = (state = initialState, action) => {
     const cartItems = state
     const product = action.payload
 
     let itemToAdd = null
     let index = -1;
 
+    function findIndex() {
+        for (let i = 0; i < cartItems.length; i++) {
+            if (cartItems[i].product.id === product.id) {
+                index = i
+            }
+        }
+    }
     switch (action.type) {
         case "ADD_TOCART":
 
-            for (let i = 0; i < cartItems.length; i++) {
-                if (cartItems[i].product.id === product.id) {
-                    index = i
-                }
-            }
-
+            findIndex()
             if (index === -1) {
                 itemToAdd = [...cartItems, { product, quantity: 1 }]
             } else {
@@ -29,12 +31,12 @@ const reducer = (state = initialState, action) => {
 
             state = itemToAdd
             localStorage.setItem("cart", JSON.stringify(state))
-            
+
             return state
         default:
             return state
     }
 }
 
-export default reducer
+export default cartReducer
 
